1161936044a6eb5d3c0245cd6bc577a3ab4241f7
Juan P. Sierra <juanpab.sierra@urosario.edu.co>
2019-10-24 21:09:52 -0500

    Html + Django

--- pagina_web/blog/views.py
+++ pagina_web/blog/views.py
@@ -1,16 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author':'Juan P. Sierra',
+        'title':'My Birthday',
+        'content':'Happy birthday Jp',
+        'date_posted':'May 24 2018',
+    },
+    {
+        'author':'Juan P. Sierra',
+        'title':'My Birthday Yesterday',
+        'content':'Not my birthday anymore.',
+        'date_posted':'May 25 2018',
+    }
+
+]
+
 def home(request):
-    return HttpResponse(
-"""
-<h1>Blog Home</h1>
-<h3> Solo quiero saber si esto tiene sentido</h3>
-""")
+    context = {
+        'posts' : posts
+    }
+    return render(request, "blog/home.html", context)
 
 def about(request):
-    return HttpResponse(
-"""
-<h1>Blog About</h1>
-<h3> Solo quiero saber si esto tiene sentido</h3>
-""")
+    content = {
+        'title': "about"
+    }
+    return render(request, "blog/about.html", content)

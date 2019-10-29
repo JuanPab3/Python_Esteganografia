5d42cbc0682f09a99496bd4bbd6bfdcb9d9c72ed
Juan P. Sierra <juanpab.sierra@urosario.edu.co>
2019-10-28 21:44:50 -0500

    aDELANTO wEB... TRABAJAR EN MANEJO DE DATOS

--- mysite/esteganew/views.py
+++ mysite/esteganew/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .esteganew import Esteganew
+from django.core.files.storage import FileSystemStorage
 
 def home(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES['pre_esteganew']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        url = fs.url(name)
+        print(url)
+        boop = Esteganew(fs, url,"perrito","mango")
+        boop.codificar()
+
+
     return render(request, "esteganew/home.html")
 
 def about(request):
-    return render(request, "esteganew/about.html", {'title': 'About'})
+        return render(request, "esteganew/about.html", {'title': 'About'})
